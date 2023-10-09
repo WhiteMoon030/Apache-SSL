2efe987eea4eeef2ea671fc025fafe6e9bd6272d
WhiteMoon030 <88623255+WhiteMoon030@users.noreply.github.com>
2023-10-10 00:12:03 +0200

    Update installer.sh
    Added shebang and an if-statement that checks if apache is already installed

--- installer.sh
+++ installer.sh
@@ -1,9 +1,13 @@
+#!/usr/bin/bash
 # Installer Skript zum installieren von Apache2
-# Auf Debian basierten Systemen über den Apt Paketmanager
+# Auf Debian basierten Systemen über den Apt-Paketmanager
 echo "Starte Installation von Apache Webserver..."
-echo "======================================="
 sleep 2
 sudo apt update
-sudo apt install apache2
-echo "======================================="
-echo "Installation Erfolgreich abgeschlossen!"
+# Prüfen ob das Paket "apache2" bereits installiert ist
+if ! dpkg -s apache2 > /dev/null; then
+  sudo apt install -y apache2
+  echo "Installation Erfolgreich abgeschlossen!"
+else
+  echo "Der Apache Webserver ist bereits installiert!"
+fi
