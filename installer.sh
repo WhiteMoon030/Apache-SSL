e69b09d7f2535dcdf2ddecb43902e9882449e68f
WhiteMoon030 <88623255+WhiteMoon030@users.noreply.github.com>
2023-10-12 14:42:38 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,30 +1,37 @@
 #!/usr/bin/bash
-# Installer Skript zum installieren von Apache2
-# Auf Debian basierten Systemen über den Apt-Paketmanager
+# Installer Skript zum installieren vom Apache-Webserver
+# Überprüfen ob das Skript als Administrator ausgeführt wird
+if [[ $(/usr/bin/id -u) -ne 0 ]]; then
+    echo "Bitte führen Sie dieses Skript mit Administratorrechten aus (sudo/root)!"
+    exit 1
+fi
+# Falls ja, Installation starten
 echo "Starte Installation von Apache Webserver..."
 sleep 2
 # Prüfen ob das Paket "apache2" bereits installiert ist
 if ! dpkg -s apache2 > /dev/null; then
   # Falls nicht -> Apache2 installieren
-  sudo apt update
-  sudo apt install -y apache2
+  apt update
+  apt install -y apache2
   echo "Installation Erfolgreich abgeschlossen!"
-  # Prüfen ob der Apache Service in systemd enabled ist
-  status=$(sudo systemctl is-enabled apache2)
+  # Prüfen ob der Apache-Service in systemd enabled ist
+  status=$(systemctl is-enabled apache2)
   if [ %status = "disabled" ]; then
-    sudo systemctl enable apache2
+    systemctl enable apache2
     echo "Apache2 enabled!"
   else
     echo "Apache2 ist bereits enabled!"
   fi
-  # Prüfen ob der Apache Service in systemd gestartet ist
-  status=$(sudo systemctl is-active apache2)
+  # Prüfen ob der Apache-Service in systemd gestartet wurde
+  status=$(systemctl is-active apache2)
   if [ %status = "inactive" ]; then
-    sudo systemctl start apache2
+    systemctl start apache2
     echo "Apache2 gestartet!"
   else
     echo "Apache2 ist bereits gestartet!"
   fi
 else
   echo "Der Apache Webserver ist bereits installiert!"
+  exit 1
 fi
+exit 0
