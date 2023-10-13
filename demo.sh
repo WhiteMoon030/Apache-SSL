d9070c2f94b95551fc189e6adc0c5bec26337159
WhiteMoon030 <88623255+WhiteMoon030@users.noreply.github.com>
2023-10-13 13:47:41 +0200

    Update demo.sh

--- demo.sh
+++ demo.sh
@@ -6,5 +6,9 @@ if [[ $(/usr/bin/id -u) -ne 0 ]]; then
     echo "Bitte führen Sie dieses Skript mit Administratorrechten aus (sudo/root)!"
     exit 1
 fi
+# Demo-html-Dateien in /var/www/html verschieben
+pfad=$(pwd)
+cp -r $pfad/demo/sicher/ /var/www/html
+cp -r $pfad/demo/unsicher/ /var/www/html
 # Virtual Hosts in der Apache konfigurieren
 exit 0
