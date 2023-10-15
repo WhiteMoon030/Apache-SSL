48721af0d147388dc60e491c0cd990111d25f0ea
WhiteMoon030 <88623255+WhiteMoon030@users.noreply.github.com>
2023-10-15 19:44:17 +0200

    Update demo.sh
    Apache Konfiguration über sed hinzugefügt

--- demo.sh
+++ demo.sh
@@ -11,4 +11,8 @@ pfad=$(pwd)
 cp -r $pfad/demo/sicher/ /var/www/html
 cp -r $pfad/demo/unsicher/ /var/www/html
 # Virtual Hosts in der Apache konfigurieren
+# -z um die Datei in einer Zeile einzulesen
+# |1 um das erste auftauchen von /var/www/html zu überschreiben, |2 für das zweite auftreten zu überschreiben
+sudo sed -i -z 's|/var/www/html|/var/www/html/unsicher|1' /etc/apache2/sites-enabled/000-default.conf
+sudo sed -i -z 's|/var/www/html|/var/www/html/sicher|2' /etc/apache2/sites-enabled/000-default.conf
 exit 0
